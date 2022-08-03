c4705b1456892a59599eab10157a2944e638a527
Mark Shercliff <me@markshercliff.com>
2022-08-03 11:03:05 +0100

    Start cards program

---
+++ code/cards/deck.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	suits := []string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	cards := deck{}
+
+	var template string
+
+	for _, suit := range suits {
+		template = " of " + suit
+
+		for _, value := range values {
+			cards = append(cards, value+template)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
---
+++ code/cards/go.mod
@@ -0,0 +1,3 @@
+module arikal.com/go/cards
+
+go 1.18
